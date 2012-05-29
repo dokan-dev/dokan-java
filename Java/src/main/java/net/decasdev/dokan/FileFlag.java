4dfb2a5a23f1e0ced90e6ded9f7d36b40611f23f
Ivan Frain <ivan.frain@ubeeko.com>
2012-05-29 16:34:33 +0200

    change Flag implementation from interface to an enum

--- Java/src/main/java/net/decasdev/dokan/FileFlag.java
+++ Java/src/main/java/net/decasdev/dokan/FileFlag.java
@@ -21,17 +21,77 @@ with this program. If not, see <http://www.gnu.org/licenses/>.
 
 package net.decasdev.dokan;
 
-public interface FileFlag {
+import java.util.EnumSet;
+import java.util.Set;
+
+public class FileFlag {
 	public final static int FLAGS_MASK= 0xFFFF0000;
-	
-	public final static int FILE_FLAG_BACKUP_SEMANTICS = 0x02000000;
-	public final static int FILE_FLAG_DELETE_ON_CLOSE = 0x04000000;
-	public final static int FILE_FLAG_NO_BUFFERING = 0x20000000;
-	public final static int FILE_FLAG_OPEN_NO_RECALL = 0x00100000;
-	public final static int FILE_FLAG_OPEN_REPARSE_POINT = 0x00200000;
-	public final static int FILE_FLAG_OVERLAPPED = 0x40000000;
-	public final static int FILE_FLAG_POSIX_SEMANTICS = 0x01000000;
-	public final static int FILE_FLAG_RANDOM_ACCESS = 0x10000000;
-	public final static int FILE_FLAG_SEQUENTIAL_SCAN = 0x08000000;
-	public final static int FILE_FLAG_WRITE_THROUGH = 0x80000000;
+
+    public enum FileFlags {
+        FILE_FLAG_BACKUP_SEMANTICS(0x02000000),
+        FILE_FLAG_DELETE_ON_CLOSE(0x04000000),
+        FILE_FLAG_NO_BUFFERING(0x20000000),
+        FILE_FLAG_OPEN_NO_RECALL(0x00100000),
+        FILE_FLAG_OPEN_REPARSE_POINT(0x00200000),
+        FILE_FLAG_OVERLAPPED(0x40000000),
+        FILE_FLAG_POSIX_SEMANTICS(0x01000000),
+        FILE_FLAG_RANDOM_ACCESS(0x10000000),
+        FILE_FLAG_SEQUENTIAL_SCAN(0x08000000),
+        FILE_FLAG_WRITE_THROUGH(0x80000000);
+
+        private int value;
+
+        FileFlags(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+    }
+
+
+    /**
+     * Translates a numeric status code into a Set of StatusFlag enums
+     * @param value
+     * @return EnumSet representing a documents status
+     */
+    public static EnumSet<FileFlags> getFlags(int value)
+    {
+        EnumSet<FileFlags> flags = EnumSet.noneOf(FileFlags.class);
+
+        for (FileFlags flag: FileFlags.values()) {
+            long flagValue = flag.getValue();
+            if ((flagValue & value) == flagValue)
+                flags.add(flag);
+        }
+
+        return flags;
+    }
+
+
+    /**
+     * Translates a set of flags enums into a numeric status code
+     * @param flags if statusFlags
+     * @return numeric representation of the document status
+     */
+    public static long getStatusValue(Set<FileFlags> flags)
+    {
+        long value=0;
+        for (FileFlags flag: flags) {
+            value |= flag.getValue();
+        }
+        return value;
+    }
+
+    public static String toString(int value) {
+        String result = new String("");
+        Set<FileFlags> flags = getFlags(value);
+
+        for (FileFlags flag: flags) {
+            result += flag.toString()+ " | ";
+        }
+
+        return result;
+    }
 }
