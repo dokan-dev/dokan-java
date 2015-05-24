d392af8e632e51635d7afe6e94b9aa1568ea49cc
unknown <Administrator@dsn-hw-herter.scc.kit.edu>
2015-05-24 12:09:21 +0200

    bugfix: rename fully-qualified class name of DokanFileInfo

--- src/main/cpp/DokanObjUtils.cpp
+++ src/main/cpp/DokanObjUtils.cpp
@@ -88,7 +88,7 @@ jobject ToDokanFileInfoJavaObject(JNIEnv* env, PDOKAN_FILE_INFO DokanFileInfo) t
 	if (DokanFileInfo == NULL)
 		return NULL;
 
-	jclass clz = env->FindClass("com/github/sherter/jdokan/DokanFileInfo");
+	jclass clz = env->FindClass("com/github/dokandev/dokanjava/DokanFileInfo");
 	if (clz == NULL) 
 		return NULL;
 	
