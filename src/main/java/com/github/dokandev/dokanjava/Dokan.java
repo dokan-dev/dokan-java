69e428da2268a4f8a724faafbe26dfe7e05f6f6c
Jonathan Hult <jonathan@jonathanhult.com>
2016-12-02 12:45:24 -0500

    Slight fix for parameterized type

--- src/main/java/com/github/dokandev/dokanjava/Dokan.java
+++ src/main/java/com/github/dokandev/dokanjava/Dokan.java
@@ -20,7 +20,7 @@ public class Dokan {
         return opts;
     }
 
-    static public int main(String mountPoint, DokanFilesystem<DokanFileHandle> fs) {
+    static public int main(String mountPoint, DokanFilesystem<? extends DokanFileHandle> fs) {
         //Runtime.getRuntime().addShutdownHook(new Thread() {
         //    @Override
         //    public void run() {
