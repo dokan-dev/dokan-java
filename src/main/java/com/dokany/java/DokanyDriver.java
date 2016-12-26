bc489891691d8d8c500228648a83c79391825aae
Jonathan Hult <jonathan@jonathanhult.com>
2016-12-26 13:04:19 -0500

    Update comments; use DeviceOptions constructor; throw mount error

--- src/main/java/com/dokany/java/DokanyDriver.java
+++ src/main/java/com/dokany/java/DokanyDriver.java
@@ -3,14 +3,19 @@ package com.dokany.java;
 import static com.dokany.java.constants.MountOptions.DEBUG_MODE;
 import static com.dokany.java.constants.MountOptions.STD_ERR_OUTPUT;
 
-import com.dokany.java.structure.Options;
+import com.dokany.java.constants.MountError;
+import com.dokany.java.structure.DeviceOptions;
 import com.sun.jna.WString;
 
+/**
+ * Main class to start and stop Dokany file system.
+ *
+ * @param <TItem> This is used in {@link com.dokany.java.FileHandle}
+ */
 public class DokanyDriver<TItem> {
 
 	private final FileSystem<TItem> fs;
-
-	private final Options driverOptions;
+	private final DeviceOptions driverOptions;
 
 	public DokanyDriver(final String mountPoint, final FileSystem<TItem> fs) {
 		this.fs = fs;
@@ -24,31 +29,53 @@ public class DokanyDriver<TItem> {
 			options |= STD_ERR_OUTPUT.val;
 		}
 
-		driverOptions = new Options(mountPoint);
-		driverOptions.Options = options;
-		driverOptions.Timeout = fs.getTimeout();
-		driverOptions.AllocationUnitSize = fs.getAllocationUnitSize();
-		driverOptions.SectorSize = fs.getSectorSize();
+		final short threadCount = 5;
+		driverOptions = new DeviceOptions(mountPoint, threadCount, options, null, fs.getTimeout(), fs.getAllocationUnitSize(), fs.getSectorSize());
 
 		System.out.println("Dokany version: " + getVersion());
 		System.out.println("Dokany driver version: " + getDriverVersion());
 	}
 
+	/**
+	 * Get driver version.
+	 *
+	 * @see {@link com.dokany.java.NativeMethods#DokanDriverVersion()}
+	 *
+	 * @return
+	 */
 	public long getDriverVersion() {
 		return NativeMethods.DokanDriverVersion();
 	}
 
+	/**
+	 * Get Dokany version.
+	 *
+	 * @see {@link com.dokany.java.NativeMethods#DokanVersion()}
+	 * @return
+	 */
 	public long getVersion() {
 		return NativeMethods.DokanVersion();
 	}
 
+	/**
+	 * Get file system.
+	 *
+	 * @return
+	 */
 	public FileSystem<TItem> getFileSystem() {
 		return fs;
 	}
 
+	/**
+	 * Calls {@link com.dokany.java.NativeMethods#DokanMain(DeviceOptions, Operations)}. Has {@link java.lang.Runtime#addShutdownHook(Thread)} which calls {@link #shutdown()}
+	 */
 	public void start() {
-		NativeMethods.DokanMain(driverOptions, new OperationsImpl<TItem>(fs) {
+		final int mountStatus = NativeMethods.DokanMain(driverOptions, new OperationsImpl<TItem>(fs) {
 		});
+
+		if (mountStatus < 0) {
+			throw new IllegalStateException(MountError.fromInt(mountStatus).name);
+		}
 
 		Runtime.getRuntime().addShutdownHook(new Thread() {
 			@Override
@@ -58,10 +85,18 @@ public class DokanyDriver<TItem> {
 		});
 	}
 
+	/**
+	 * Calls {@link #stop(String)}.
+	 */
 	public void shutdown() {
 		stop(driverOptions.MountPoint.toString());
 	}
 
+	/**
+	 * Calls {@link com.dokany.java.NativeMethods#DokanUnmount(char)} and {@link com.dokany.java.NativeMethods#DokanRemoveMountPoint(WString)}
+	 *
+	 * @param mountPoint
+	 */
 	public static void stop(final String mountPoint) {
 		System.out.println("Unmount and shutdown: " + mountPoint);
 		NativeMethods.DokanUnmount(mountPoint.charAt(0));
