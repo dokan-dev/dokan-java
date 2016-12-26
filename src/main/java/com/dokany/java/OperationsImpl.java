d32a75e19d329cc33e5fdcb1dde57b10adf8748d
Jonathan Hult <jonathan@jonathanhult.com>
2016-12-26 02:15:18 -0500

    Switch createFile logic - not yet tested

--- src/main/java/com/dokany/java/OperationsImpl.java
+++ src/main/java/com/dokany/java/OperationsImpl.java
@@ -1,16 +1,14 @@
 package com.dokany.java;
 
-import static com.dokany.java.constants.CreationDisposition.CREATE_NEW;
-import static com.dokany.java.constants.CreationDisposition.TRUNCATE_EXISTING;
-import static com.dokany.java.constants.ErrorCodes.ERROR_ALREADY_EXISTS;
 import static com.dokany.java.constants.ErrorCodes.ERROR_FILE_NOT_FOUND;
 import static com.dokany.java.constants.ErrorCodes.ERROR_READ_FAULT;
-import static com.dokany.java.constants.ErrorCodes.ERROR_SUCCESS;
 import static com.dokany.java.constants.ErrorCodes.ERROR_WRITE_FAULT;
-import static com.dokany.java.constants.ErrorCodes.ObjectNameCollision;
+import static com.dokany.java.constants.ErrorCodes.OBJECT_NAME_COLLISION;
+import static com.dokany.java.constants.ErrorCodes.SUCCESS;
 import static com.dokany.java.constants.NtStatus.FileInvalid;
 import static com.dokany.java.constants.NtStatus.Success;
 import static com.dokany.java.constants.NtStatus.Unsuccessful;
+import static com.dokany.java.constants.WinError.ERROR_ALREADY_EXISTS;
 
 import java.io.FileNotFoundException;
 import java.nio.file.FileAlreadyExistsException;
@@ -18,8 +16,11 @@ import java.nio.file.FileSystems;
 import java.util.Arrays;
 import java.util.List;
 
-import com.dokany.java.FileSystem.OpenFileResult;
+import com.dokany.java.constants.CreationDisposition;
+import com.dokany.java.constants.FileAttribute;
+import com.dokany.java.constants.FileSystemFeatures;
 import com.dokany.java.structure.ByHandleFileInformation;
+import com.dokany.java.structure.FileInfo;
 import com.dokany.java.structure.FileInfoRaw;
 import com.dokany.java.structure.FileTime;
 import com.dokany.java.structure.Win32FindData;
@@ -74,78 +75,79 @@ abstract class OperationsImpl<TItem> extends Operations {
 		        final int rawCreateOptions,
 		        final FileInfoRaw dokanFileInfo) {
 
-			// if (rawFileName.toString().equals("\\") ||
-			// rawFileName.toString().equals("\\desktop.ini")) {
-			// return FileInvalid.val;
-			// }
+			if (isSkipFile(rawFileName)) {
+				return FileInvalid.val;
+			}
 
-			long res = -1;
+			if (rawFileName.equals("\\")) {
+				return ERROR_ALREADY_EXISTS.val;
+			}
+
+			long toReturn = -1;
+
+			final String fileName = rawFileName.toString();
+			final CreationDisposition disposition = CreationDisposition.fromInt(rawCreateDisposition);
 			final boolean isDirectory = dokanFileInfo._isDirectory != 0;
-			System.out.println("--------------------------------------------");
+			final FileAttribute attributes = FileAttribute.fromInt(rawFileAttributes);
+
+			TItem item;
+
 			try {
-				System.out.println("%%%%%%%%%%%%%% CREATEFILE: rawFileName=" + rawFileName.toString() + " : rawDesiredAccess=" + rawDesiredAccess
-				        + " : rawFileAttributes=" + rawFileAttributes + " : rawShareAccess=" + rawShareAccess
-				        + " : rawCreateDisposition=" + rawCreateDisposition + " : rawCreateOptions=" + rawCreateOptions + " : isDirectory=" + isDirectory);
-				final OpenFileResult<TItem> result = fs.createFile(rawFileName.toString(), securityContext.getValue(),
-				        rawDesiredAccess, rawFileAttributes, rawShareAccess, rawCreateDisposition, rawCreateOptions, isDirectory);
+				item = fs.findExisting(fileName, isDirectory);
 
-				System.out.println("result: " + result);
-				System.out.println("dokanFileInfo: " + dokanFileInfo);
+				switch (disposition) {
 
-				dokanFileInfo._context = FileSystem.fileHandles.allocateFileHandle(result.handle);
-				dokanFileInfo.write();
-
-				// This is a directory
-				if (isDirectory) {
-					if (result.exists) {
-						res = ERROR_SUCCESS.val;
-					} else {
-						res = ERROR_FILE_NOT_FOUND.val;
+				case CREATE_NEW: {
+					if (Utils.isNotNull(item)) {
+						toReturn = OBJECT_NAME_COLLISION.val;
 					}
-				} else {
-					// This is a file
-					if ((rawCreateDisposition == CREATE_NEW.val) || (rawCreateDisposition == TRUNCATE_EXISTING.val)) {
-						if (result.exists) {
-							res = ObjectNameCollision.val;
-						} else {
-							res = ERROR_SUCCESS.val;
-						}
+					break;
+				}
 
-					} else {
-						if (result.exists) {
-							res = ERROR_ALREADY_EXISTS.val;
-						} else {
-							res = ERROR_FILE_NOT_FOUND.val;
-						}
+				case CREATE_ALWAYS:
+				case OPEN_ALWAYS: {
+					break;
+				}
+
+				case OPEN_EXISTING: {
+					if (Utils.isNull(item)) {
+						toReturn = ERROR_FILE_NOT_FOUND.val;
 					}
+					break;
 				}
-			} catch (final FileAlreadyExistsException t) {
-				if (rawCreateDisposition == CREATE_NEW.val) {
-					res = ObjectNameCollision.val;
+
+				case TRUNCATE_EXISTING: {
+					if (Utils.isNull(item)) {
+						toReturn = ERROR_FILE_NOT_FOUND.val;
+					}
+					// TODO: need to truncate somehow
+					break;
 				}
-				res = ERROR_SUCCESS.val;
+				}
+				if (toReturn == 0) {
+					item = fs.createFile(fileName, disposition, rawCreateOptions, isDirectory, attributes);
+					dokanFileInfo._context = fs.createHandle(fileName).getID();
+					dokanFileInfo.write();
+					toReturn = SUCCESS.val;
+				}
 			} catch (final Throwable t) {
-				res = exceptionToErrorCode(t);
+				toReturn = exceptionToErrorCode(t);
 			}
 
-			if (res < 0) {
-				System.out.println("TEST");
-			}
-
-			System.out.println(" CREATEFILE -> " + res);
-
-			return res;
+			return toReturn;
 		}
-
 	}
 
 	private class Mounted implements Operations.MountedDelegate {
 		@Override
 		public long callback(final FileInfoRaw rawFileInfo) {
-			System.out.println("Mounted");
+
 			try {
 				fs.mounted();
 
+				if (fs.isDefaultLog()) {
+					System.out.println("Dokany File System mounted");
+				}
 				return Success.val;
 			} catch (final Throwable t) {
 				return exceptionToErrorCode(t);
@@ -156,9 +158,13 @@ abstract class OperationsImpl<TItem> extends Operations {
 	private class Unmounted implements Operations.UnmountedDelegate {
 		@Override
 		public long callback(final FileInfoRaw rawFileInfo) {
-			System.out.println("Unmounted");
+
 			try {
 				fs.unmounted();
+
+				if (fs.isDefaultLog()) {
+					System.out.println("Dokany File System unmounted");
+				}
 
 				return Success.val;
 			} catch (final Throwable t) {
@@ -172,10 +178,17 @@ abstract class OperationsImpl<TItem> extends Operations {
 		public void callback(final WString fileName,
 		        final FileInfoRaw rawFileInfo) {
 
-			System.out.println("Cleanup: " + fileName + " : " + rawFileInfo);
+			if (isSkipFile(fileName)) {
+				return;
+			}
 
 			try {
 				fs.cleanup(fs.getFileHandle(fileName, rawFileInfo._context));
+
+				if (fs.isDefaultLog()) {
+					System.out.println("Cleaned up: " + fileName);
+				}
+
 			} catch (final Throwable t) {
 				t.printStackTrace();
 			}
@@ -186,10 +199,16 @@ abstract class OperationsImpl<TItem> extends Operations {
 		@Override
 		public void callback(final WString fileName, final FileInfoRaw rawFileInfo) {
 
-			System.out.println("CloseFile");
+			if (isSkipFile(fileName)) {
+				return;
+			}
 
 			try {
 				fs.closeFile(fs.getFileHandle(fileName, rawFileInfo._context));
+
+				if (fs.isDefaultLog()) {
+					System.out.println("Closed file: " + fileName.toString());
+				}
 			} catch (final FileNotFoundException e) {
 			} catch (final Throwable t) {
 				t.printStackTrace();
@@ -208,14 +227,21 @@ abstract class OperationsImpl<TItem> extends Operations {
 		        final int rawFileSystemNameSize,
 		        final FileInfoRaw rawFileInfo) {
 
-			System.out.println("GetVolumeInformation");
-
 			try {
 				volumeNameBuffer.setWideString(0L, Utils.trimStrToSize(fs.getVolumeName(), volumeNameSize));
+				System.out.println("Volume name: " + fs.getVolumeName());
+
 				rawFileSystemNameBuffer.setWideString(0L, Utils.trimStrToSize(fs.getFileSystemName(), rawFileSystemNameSize));
-				rawVolumeSerialNumber.setValue(fs.getSerialNumber());
+				System.out.println("File system name: " + fs.getFileSystemName());
+
+				rawVolumeSerialNumber.setValue(fs.getVolumeSerialNumber());
+				System.out.println("Serial number: " + fs.getVolumeSerialNumber());
+
 				rawMaximumComponentLength.setValue(fs.getMaxComponentLength());
+				System.out.println("Max component length: " + fs.getMaxComponentLength());
+
 				rawFileSystemFlags.setValue(fs.getFileSystemFeatures());
+				System.out.println("File system features: " + FileSystemFeatures.fromInt(fs.getFileSystemFeatures()));
 
 				return Success.val;
 			} catch (final Throwable t) {
@@ -243,7 +269,9 @@ abstract class OperationsImpl<TItem> extends Operations {
 
 	private class FindFiles implements Operations.FindFilesDelegate {
 		@Override
-		public long callback(final WString fileName, final Operations.FillWin32FindData rawFillFindData, final FileInfoRaw rawFileInfo) {
+		public long callback(final WString fileName,
+		        final Operations.FillWin32FindData rawFillFindData,
+		        final FileInfoRaw rawFileInfo) {
 
 			System.out.println("FindFiles");
 
@@ -350,14 +378,23 @@ abstract class OperationsImpl<TItem> extends Operations {
 		        final ByHandleFileInformation handleFileInfo,
 		        final FileInfoRaw rawFileInfo) {
 
-			System.out.println("GetFileInformation");
-
-			if (fileName.toString().equals("\\") || fileName.toString().equals("\\desktop.ini")) {
+			if (isSkipFile(fileName)) {
 				return FileInvalid.val;
 			}
 
 			try {
-				fs.getFileInformation(fs.getFileHandle(fileName, rawFileInfo._context));
+				System.out.println("GetFileInformation: " + fileName);
+				final FileHandle<TItem> handle = fs.getFileHandle(fileName, rawFileInfo._context);
+				final FileInfo fi = fs.getFileInformation(handle);
+				final ByHandleFileInformation bhfi;
+
+				// if (fileName.equals("\\")) {
+				final int numberOfLinks = 1;
+				final int volumeSerialNumber = fs.getVolumeSerialNumber();
+				bhfi = fi.toByHandleFileInformation(numberOfLinks, volumeSerialNumber);
+				// } else {
+				// }
+				handleFileInfo.copyTo(bhfi);
 
 				return Success.val;
 			} catch (final Throwable t) {
@@ -574,9 +611,12 @@ abstract class OperationsImpl<TItem> extends Operations {
 		        final long byteOffset,
 		        final long length,
 		        final FileInfoRaw rawFileInfo) {
+
 			System.out.println("UnlockFile");
+
 			try {
 				fs.unlockFile(fs.getFileHandle(fileName, rawFileInfo._context), byteOffset, length);
+
 				return Success.val;
 			} catch (final Throwable t) {
 				return exceptionToErrorCode(t);
@@ -584,7 +624,7 @@ abstract class OperationsImpl<TItem> extends Operations {
 		}
 	}
 
-	protected static class Win32FindStreamData extends Structure implements Operations.Win32FindStreamData {
+	static class Win32FindStreamData extends Structure implements Operations.Win32FindStreamData {
 		public long length;
 		public char[] cFileName = new char[Win32FindData.MAX_PATH + 36];
 
@@ -621,5 +661,15 @@ abstract class OperationsImpl<TItem> extends Operations {
 
 		t.printStackTrace();
 		return defaultCode;
+	}
+
+	private boolean isSkipFile(final WString fileName) {
+		boolean toReturn = false;
+		// fileName.toString().equals("\\") ||
+		if (fileName.toString().equals("\\desktop.ini")) {
+			System.out.println("Skipping file: " + fileName);
+			toReturn = true;
+		}
+		return toReturn;
 	}
 }
