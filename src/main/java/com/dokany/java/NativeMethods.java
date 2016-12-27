9f8b1bcc9f3128c3403e433518c3bf6bde32a8a4
Jonathan Hult <jonathan@jonathanhult.com>
2016-12-27 09:55:48 -0500

    Add final

--- src/main/java/com/dokany/java/NativeMethods.java
+++ src/main/java/com/dokany/java/NativeMethods.java
@@ -7,11 +7,11 @@ import com.sun.jna.WString;
 import com.sun.jna.ptr.IntByReference;
 
 /**
- * Native API to the kernel Dokany driver.
+ * Native API to the kernel Dokany driver. This is an internal class and should not used directly.
  *
  */
-class NativeMethods {
-	static final String DOKAN_DLL = "dokan1";
+final class NativeMethods {
+	final static String DOKAN_DLL = "dokan1";
 
 	static {
 		Native.register(DOKAN_DLL);
@@ -24,21 +24,21 @@ class NativeMethods {
 	 * @param operations Instance of {@link com.dokany.java.Operations} that will be called for each request made by the kernel.
 	 * @return {@link com.dokany.java.constants.MountError}.
 	 */
-	static native int DokanMain(DeviceOptions options, Operations operations);
+	final static native int DokanMain(DeviceOptions options, Operations operations);
 
 	/**
 	 * Get the version of Dokan.
 	 *
 	 * @return The version number without the dots.
 	 */
-	static native long DokanVersion();
+	final static native long DokanVersion();
 
 	/**
 	 * Get the version of the Dokan driver.
 	 *
 	 * @return The version number without the dots.
 	 */
-	static native long DokanDriverVersion();
+	final static native long DokanDriverVersion();
 
 	/**
 	 * Unmount a dokan device from a driver letter.
@@ -46,7 +46,7 @@ class NativeMethods {
 	 * @param driveLetter Dokan driver letter to unmount.
 	 * @return Trueif device was unmount or false in case of failure or device not found.
 	 */
-	static native boolean DokanUnmount(char driveLetter);
+	final static native boolean DokanUnmount(char driveLetter);
 
 	/**
 	 * Unmount a dokan device from a mount point
@@ -54,7 +54,7 @@ class NativeMethods {
 	 * @param mountPoint Mount point to unmount Z or Z: or Z:\\ or Z:\MyMountPoint.
 	 * @return
 	 */
-	static native boolean DokanRemoveMountPoint(WString mountPoint);
+	final static native boolean DokanRemoveMountPoint(WString mountPoint);
 
 	/**
 	 * Extends the time out of the current IO operation in driver.
@@ -63,7 +63,7 @@ class NativeMethods {
 	 * @param rawFileInfo {@link com.dokany.java.structure.FileInfoRaw} of the operation to extend.
 	 * @return if the operation was successful or not.
 	 */
-	static native boolean DokanResetTimeout(long timeout, FileInfoRaw rawFileInfo);
+	final static native boolean DokanResetTimeout(long timeout, FileInfoRaw rawFileInfo);
 
 	/**
 	 * Get the handle to Access Token.
@@ -71,7 +71,7 @@ class NativeMethods {
 	 * @param rawFileInfo {@link com.dokany.java.structure.FileInfoRaw} of the operation.
 	 * @return A handle to the account token for the user on whose behalf the code is running.
 	 */
-	static native IntByReference DokanOpenRequestorToken(FileInfoRaw rawFileInfo);
+	final static native IntByReference DokanOpenRequestorToken(FileInfoRaw rawFileInfo);
 
 	/**
 	 * Convert {@link com.dokany.java.structure.Operations.ZwCreateFileDelegate} parameters to CreateFile parameters.
@@ -82,6 +82,6 @@ class NativeMethods {
 	 * @param outFileAttributesAndFlags
 	 * @param outCreationDisposition
 	 */
-	static native void DokanMapKernelToUserCreateFileFlags(long fileAttributes, long createOptions, long createDisposition, IntByReference outFileAttributesAndFlags,
+	final static native void DokanMapKernelToUserCreateFileFlags(long fileAttributes, long createOptions, long createDisposition, IntByReference outFileAttributesAndFlags,
 	        IntByReference outCreationDisposition);
 }
