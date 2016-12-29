b86c0a7565c6f300048a0429823ccb4df0ea5f3a
Jonathan Hult <jonathan@jonathanhult.com>
2016-12-29 18:12:55 -0500

    Can now retrieve info directly from node

--- src/main/java/com/dokany/java/examples/memoryfs/NodeFileHandle.java
+++ src/main/java/com/dokany/java/examples/memoryfs/NodeFileHandle.java
@@ -1,28 +1,17 @@
 package com.dokany.java.examples.memoryfs;
 
-import static com.dokany.java.constants.FileAttribute.FILE_ATTRIBUTE_DIRECTORY;
-import static com.dokany.java.constants.FileAttribute.FILE_ATTRIBUTE_NORMAL;
-
 import java.nio.file.Path;
 
 import com.dokany.java.FileHandle;
-import com.dokany.java.constants.FileAttribute;
 import com.dokany.java.structure.ByHandleFileInfo;
-import com.dokany.java.structure.ByHandleFileInfo.FileInfoBuilder;
 
 class NodeFileHandle extends FileHandle<Node> {
-	NodeFileHandle(final Path path, final Node item) {
-		super(path, item);
+	NodeFileHandle(final Path path, final Node node) {
+		super(path, node);
 	}
 
-	static ByHandleFileInfo getFileInfo(final Node item) {
-		final Path path = item.getPath();
-		final FileAttribute attributes = item.isDirectory() ? FILE_ATTRIBUTE_DIRECTORY : FILE_ATTRIBUTE_NORMAL;
-		final long size = item.getSize();
-		final FileInfoBuilder builder = new ByHandleFileInfo.FileInfoBuilder(path);
-		builder.size(size);
-		builder.attributes(attributes);
-		return builder.build();
+	static ByHandleFileInfo getFileInfo(final Node node) {
+		return node.getInfo();
 	}
 
 	@Override
