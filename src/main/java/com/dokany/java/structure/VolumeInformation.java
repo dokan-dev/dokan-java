ef48210c993580525fce77ec38fcbfd6f84b6a5c
Jonathan Hult <jonathan@jonathanhult.com>
2017-01-03 14:13:34 -0500

    Add new classes to handle data in OO fashion

---
+++ src/main/java/com/dokany/java/structure/VolumeInformation.java
@@ -0,0 +1,62 @@
+package com.dokany.java.structure;
+
+import static com.dokany.java.constants.FileSystemFeatures.CASE_PRESERVED_NAMES;
+
+import com.dokany.java.constants.FileSystemFeatures;
+
+public class VolumeInformation {
+
+	private final int maxComponentLength;
+	private final int serialNumber;
+	private final String volumeName;
+	private final String fileSystemName;
+	private final FileSystemFeatures fileSystemFeatures;
+
+	public VolumeInformation() {
+		maxComponentLength = 256;
+		serialNumber = 0x12345678;
+		volumeName = "VOLUME";
+		fileSystemName = "DOKANY";
+		fileSystemFeatures = CASE_PRESERVED_NAMES;
+	}
+
+	public int getMaxComponentLength() {
+		return maxComponentLength;
+	}
+
+	/**
+	 * Default is 0x00000000
+	 *
+	 * @return
+	 */
+	public int getVolumeSerialNumber() {
+		return serialNumber;
+	}
+
+	/**
+	 * Default is VOLUME;
+	 *
+	 * @return
+	 */
+	public String getVolumeName() {
+		return volumeName;
+	}
+
+	/**
+	 * Default is DOKANY.
+	 *
+	 * @return
+	 */
+	public String getFileSystemName() {
+		return fileSystemName;
+	}
+
+	/**
+	 * Default is FileSystemFeatures.CasePreservedNames
+	 *
+	 * @return
+	 */
+	public FileSystemFeatures getFileSystemFeatures() {
+		return fileSystemFeatures;
+	}
+}
