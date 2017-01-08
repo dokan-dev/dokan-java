e275146db38f8b6709ab73e0b4e41d502568c9d9
Jonathan Hult <jonathan@jonathanhult.com>
2017-01-08 12:03:30 -0500

    Allow passing in exception

--- src/main/java/com/dokany/java/DokanyException.java
+++ src/main/java/com/dokany/java/DokanyException.java
@@ -1,5 +1,7 @@
 package com.dokany.java;
 
+import java.io.IOException;
+
 import com.dokany.java.constants.ErrorCodes;
 import com.dokany.java.constants.WinError;
 
@@ -7,21 +9,24 @@ public class DokanyException extends RuntimeException {
 	/**
 	 *
 	 */
-	private static final long serialVersionUID = 217360542205521310L;
+	private static final long serialVersionUID = -862591089502909563L;
+	/**
+	*
+	*/
 	public final int val;
 
-	public DokanyException(final long errorCode) {
+	public DokanyException(final long errorCode, final IOException exception) {
 		if ((errorCode < 0) || (errorCode > 4294967295L)) {
-			throw new IllegalArgumentException("error code is not in range [0, 4294967295]");
+			throw new IllegalArgumentException("error code (" + errorCode + ") is not in range [0, 4294967295]", exception);
 		}
 		val = (int) errorCode;
 	}
 
-	public DokanyException(final WinError errorCode) {
-		this(errorCode.val);
+	public DokanyException(final WinError errorCode, final IOException exception) {
+		this(errorCode.val, exception);
 	}
 
-	public DokanyException(final ErrorCodes errorCode) {
-		this(errorCode.val);
+	public DokanyException(final ErrorCodes errorCode, final IOException exception) {
+		this(errorCode.val, exception);
 	}
 }
