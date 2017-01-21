ac447897545c1185549fd58eddea18b1a07e3beb
Jonathan Hult <jonathan@jonathanhult.com>
2017-01-20 21:42:18 -0500

    Use same variable names as in dokan.h; add public constructor

--- src/main/java/com/dokany/java/structure/DokanyFileInfo.java
+++ src/main/java/com/dokany/java/structure/DokanyFileInfo.java
@@ -4,63 +4,107 @@ import java.util.Arrays;
 import java.util.List;
 
 import com.sun.jna.Structure;
-import com.sun.jna.ptr.IntByReference;
 
+import lombok.ToString;
+
+/**
+ * From dokan.h
+ *
+ * @see {@link https://github.com/dokan-dev/dokany/blob/master/dokan/dokan.h}
+ *
+ */
+@ToString
 public class DokanyFileInfo extends Structure implements Structure.ByReference {
-	public long _context;
-	public long _dokanContext;
-	public IntByReference _dokanOptions;
-	public int _processId;
-	public byte _isDirectory;
-	public byte _deleteOnClose;
-	public byte _pagingIo;
-	public byte _synchronousIo;
-	public byte _noCache;
-	public byte _writeToEndOfFile;
+	/**
+	 * This can be used to carry information between operation. HANDLE This can be whatever type such as {@link com.sun.jna.platform.win32.WinNT.HANDLE},
+	 * {@link com.sun.jna.Structure}, {@link com.sun.jna.ptr.IntByReference}, {@link com.sun.jna.Pointer} that will help the implementation understand the request context of the
+	 * event.
+	 */
+	public long Context;
+
+	/**
+	 * Reserved. Used internally by Dokany library. Never modify.
+	 */
+	public long DokanContext;
+
+	/**
+	 * A pointer to {@link DeviceOptions} which was passed to {@link com.dokany.java.NativeMethods#DokanMain}.
+	 */
+	public DeviceOptions DokanOptions;
+
+	/**
+	 * Process id for the thread that originally requested a given I/O operation.
+	 */
+	public int ProcessId;
+
+	/**
+	 * Requesting a directory file.
+	 *
+	 * Must be set in {@link com.dokany.java.DokanyOperations#ZwCreateFile} if the file object appears to be a directory.
+	 */
+	public byte IsDirectory;
+
+	/**
+	 * Flag if the file has to be delete during {@link com.dokany.java.DokanyOperations#Cleanup} event.
+	 */
+	public byte DeleteOnClose;
+
+	/**
+	 * Read or write is paging IO.
+	 */
+	public byte PagingIo;
+
+	/**
+	 * Read or write is synchronous IO.
+	 */
+	public byte SynchronousIo;
+
+	/**
+	 * Read or write directly from data source without cache.
+	 */
+	public byte Nocache;
+
+	/**
+	 * If true, write to the current end of file instead of using the Offset parameter.
+	 */
+	public byte WriteToEndOfFile;
 
 	@Override
 	protected List<String> getFieldOrder() {
-		return Arrays.asList("_context", "_dokanContext", "_dokanOptions", "_processId", "_isDirectory", "_deleteOnClose", "_pagingIo", "_synchronousIo", "_noCache",
-		        "_writeToEndOfFile");
-	}
-
-	@Override
-	public String toString() {
-		return "DokanFileInfo{" +
-		        "_context=" + _context +
-		        ", _dokanContext=" + _dokanContext +
-		        ", _dokanOptions=" + _dokanOptions +
-		        ", _processId=" + _processId +
-		        ", _isDirectory=" + _isDirectory +
-		        ", _deleteOnClose=" + _deleteOnClose +
-		        ", _pagingIo=" + _pagingIo +
-		        ", _synchronousIo=" + _synchronousIo +
-		        ", _noCache=" + _noCache +
-		        ", _writeToEndOfFile=" + _writeToEndOfFile +
-		        '}';
+		return Arrays.asList(
+		        "Context",
+		        "DokanContext",
+		        "DokanOptions",
+		        "ProcessId",
+		        "IsDirectory",
+		        "DeleteOnClose",
+		        "PagingIo",
+		        "SynchronousIo",
+		        "Nocache",
+		        "WriteToEndOfFile");
 	}
 
 	public final boolean isDirectory() {
-		return _isDirectory != 0;
+		return IsDirectory != 0;
 	}
 
 	public final boolean deleteOnClose() {
-		return _deleteOnClose != 0;
+		return DeleteOnClose != 0;
 	}
 
 	public final boolean pagingIo() {
-		return _pagingIo != 0;
+		return PagingIo != 0;
 	}
 
 	public final boolean synchronousIo() {
-		return _synchronousIo != 0;
+		return SynchronousIo != 0;
 	}
 
 	public final boolean noCache() {
-		return _noCache != 0;
+		return Nocache != 0;
 	}
 
 	public final boolean writeToEndOfFile() {
-		return _writeToEndOfFile != 0;
+		return WriteToEndOfFile != 0;
 	}
 }
