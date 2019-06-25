0c601e3040c5e09ae2383e010f18a3c449f0b9d8
Armin Schrenk <armin.schrenk@zoho.eu>
2019-06-25 18:54:33 +0200

    added constructors to new exceptions

--- src/main/java/dev/dokan/dokan_java/MountFailedException.java
+++ src/main/java/dev/dokan/dokan_java/MountFailedException.java
@@ -1,4 +1,16 @@
 package dev.dokan.dokan_java;
 
-public class MountFailedException extends Exception{
+public class MountFailedException extends Exception {
+
+    public MountFailedException(String msg) {
+        super(msg);
+    }
+
+    public MountFailedException(String msg, Throwable cause) {
+        super(msg, cause);
+    }
+
+    public MountFailedException(Throwable cause) {
+        super(cause);
+    }
 }
