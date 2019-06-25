75fbb79236294babcd9641631b447d8d4776b8d5
Armin Schrenk <armin.schrenk@zoho.eu>
2019-06-25 18:45:39 +0200

    Introducing exceptions for failed mount and unmount operation

---
+++ src/main/java/dev/dokan/dokan_java/MountFailedException.java
@@ -0,0 +1,4 @@
+package dev.dokan.dokan_java;
+
+public class MountFailedException extends Exception{
+}
---
+++ src/main/java/dev/dokan/dokan_java/UnmountFailedException.java
@@ -0,0 +1,4 @@
+package dev.dokan.dokan_java;
+
+public class UnmountFailedException extends RuntimeException {
+}
