5b6e892bccf61574d5455795f1fe771ce990b07a
infeo <armin.schrenk@zoho.eu>
2019-07-12 22:32:46 +0200

    formatting

--- src/main/java/dev/dokan/dokan_java/Mountable.java
+++ src/main/java/dev/dokan/dokan_java/Mountable.java
@@ -16,16 +16,16 @@ public interface Mountable extends AutoCloseable {
     /**
      * Mount this object on a mount point with the given options.
      *
-     * @param mountPoint Path pointing to an empty Directory or unused drive letter
-     * @param volumeName The displayed name of the volume (only important in combination with a drive letter)
+     * @param mountPoint         Path pointing to an empty Directory or unused drive letter
+     * @param volumeName         The displayed name of the volume (only important in combination with a drive letter)
      * @param volumeSerialnumber the serial number of the volume
-     * @param blocking If true the mount and further file system calls are foreground operations and thus will block this thread
-     * @param timeout Timeout after which a not processed file system call is canceled
+     * @param blocking           If true the mount and further file system calls are foreground operations and thus will block this thread
+     * @param timeout            Timeout after which a not processed file system call is canceled
      * @param allocationUnitSize the size of the smallest allocatable space in bytes
-     * @param sectorSize the sector size
+     * @param sectorSize         the sector size
      * @param UNCName
-     * @param threadCount the number of threads spawned for processing filesystem calls
-     * @param options an {@link EnumIntegerSet} containing {@link MountOption}s
+     * @param threadCount        the number of threads spawned for processing filesystem calls
+     * @param options            an {@link EnumIntegerSet} containing {@link MountOption}s
      */
     void mount(Path mountPoint, String volumeName, int volumeSerialnumber, boolean blocking, long timeout, long allocationUnitSize, long sectorSize, String UNCName, short threadCount, EnumIntegerSet<MountOption> options);
 
