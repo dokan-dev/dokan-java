bbbf001d3f2fa9ba6eebb77f0b1f58d84be1d472
infeo <armin.schrenk@zoho.eu>
2019-09-21 18:31:28 +0200

    fixing severe bug where CreateDispositions where wrong

--- src/main/java/dev/dokan/dokan_java/constants/microsoft/CreateDispositions.java
+++ src/main/java/dev/dokan/dokan_java/constants/microsoft/CreateDispositions.java
@@ -2,14 +2,15 @@ package dev.dokan.dokan_java.constants.microsoft;
 
 public final class CreateDispositions {
 
-    public final static int FILE_SUPERSEDE = 1;
+    public final static int FILE_SUPERSEDE = 0;
+    public final static int FILE_OPEN = 1;
     public final static int FILE_CREATE = 2;
-    public final static int FILE_OPEN = 3;
-    public final static int FILE_OPEN_IF = 4;
-    public final static int FILE_OVERWRITE = 5;
-    public final static int FILE_OVERWRITE_IF = 6;
+    public final static int FILE_OPEN_IF = 3;
+    public final static int FILE_OVERWRITE = 4;
+    public final static int FILE_OVERWRITE_IF = 5;
 
     private CreateDispositions() {
+
     }
 
 }
