ee61d0f2f7e32b785bd90ce64ddee30b64d38fd2
Armin Schrenk <armin.schrenk@zoho.eu>
2020-05-20 22:03:20 +0200

    implement the DokanIOSecurityContext

---
+++ src/main/java/dev/dokan/dokan_java/structure/DokanIOSecurityContext.java
@@ -0,0 +1,29 @@
+package dev.dokan.dokan_java.structure;
+
+
+import com.sun.jna.Structure;
+import com.sun.jna.WString;
+import com.sun.jna.platform.win32.WinBase;
+
+
+/**
+ * The DokanIOSecurityContext contains the Dokan specific security context of the Windows kernel create request.
+ * It is a parameter in the {@link dev.dokan.dokan_java.DokanFileSystem#zwCreateFile(WString, WinBase.SECURITY_ATTRIBUTES, int, int, int, int, int, DokanFileInfo)} function.
+ *
+ * @see <a href="https://docs.microsoft.com/en-us/windows-hardware/drivers/ddi/wdm/ns-wdm-_io_security_context?redirectedfrom=MSDN">Microsoft documentation</a> of the original structure
+ * @see <a href="https://github.com/dokan-dev/dokany/blob/master/sys/public.h>Definition in {@code public.h}</a> of the Dokany project.
+ */
+@Structure.FieldOrder({"AccessState, DesiredAccess"})
+public class DokanIOSecurityContext extends Structure implements Structure.ByReference {
+
+	/**
+	 * Dokan ACCESS_STATE structure that contains the object's subject context, granted access types, and remaining desired access types.
+	 */
+	public DokanAccessState AccessState;
+
+	/**
+	 * An ACCESS_MASK value that expresses the access rights that are requested in the IRP_MJ_CREATE request.
+	 */
+	public int DesiredAccess;
+
+}
