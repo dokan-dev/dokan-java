990da50f32fc79cd2e0ad0817d5bc514549ba85c
Armin Schrenk <armin.schrenk@zoho.eu>
2020-05-20 21:40:07 +0200

    add comment that maybe this should just be a pointer

--- src/main/java/dev/dokan/dokan_java/structure/UnicodeString.java
+++ src/main/java/dev/dokan/dokan_java/structure/UnicodeString.java
@@ -28,6 +28,7 @@ public class UnicodeString extends Structure implements Structure.ByReference {
 	 * Pointer to a buffer used to contain a string of wide characters.
 	 *
 	 * Just kiddin, we directly convert it to a WString.
+	 * TODO: maybe just make a reference here!
 	 */
 	public WString Buffer;
 
