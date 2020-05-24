e01b3767cf2afc6ca5036296da58598ed92e34ec
JaniruTEC <JaniruTEC@gmail.com>
2020-05-24 15:48:16 +0200

    Added additional delegates to MaskValueEnum

--- src/main/java/dev/dokan/dokan_java/conv/MaskValueEnum.java
+++ src/main/java/dev/dokan/dokan_java/conv/MaskValueEnum.java
@@ -3,8 +3,16 @@ package dev.dokan.dokan_java.conv;
 
 public interface MaskValueEnum extends EnumInteger {
 
+    static <T extends Enum<T> & MaskValueEnum> T enumFromInt(final int value, final T[] enumValues) {
+        return EnumInteger.enumFromInt(value, enumValues);
+    }
+
     static <T extends Enum<T> & MaskValueEnum> MaskValueSet<T> getSetFromInt(final int intValue, final T[] allEnumValues) {
         return MaskValueSet.getSetFromInt(intValue, allEnumValues);
+    }
+
+    static <T extends Enum<T> & MaskValueEnum> MaskValueSet<T> getSetFromInt(final int intValue, final Class<T> type) {
+        return MaskValueSet.getSetFromInt(intValue, type);
     }
 
     default int getMaskValue() {
