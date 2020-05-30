7975114ce13382ea4ed326886a864924d8f4dcdf
JaniruTEC <JaniruTEC@gmail.com>
2020-05-30 18:04:44 +0200

    Removed delegating methods from EnumInteger
    See: https://github.com/dokan-dev/dokan-java/pull/41#discussion_r432771434

--- src/main/java/dev/dokan/dokan_java/masking/MaskValueEnum.java
+++ src/main/java/dev/dokan/dokan_java/masking/MaskValueEnum.java
@@ -3,18 +3,6 @@ package dev.dokan.dokan_java.masking;
 
 public interface MaskValueEnum extends EnumInteger {
 
-    static <T extends Enum<T> & MaskValueEnum> T maskEnumFromInt(final int maskingValue, final T[] enumValues) {
-        return EnumInteger.enumFromInt(maskingValue, enumValues);
-    }
-
-    static <T extends Enum<T> & MaskValueEnum> MaskValueSet<T> maskValueSet(final int mask, final T[] allEnumValues) {
-        return MaskValueSet.maskValueSet(mask, allEnumValues);
-    }
-
-    static <T extends Enum<T> & MaskValueEnum> MaskValueSet<T> maskValueSet(final int mask, final Class<T> type) {
-        return MaskValueSet.maskValueSet(mask, type);
-    }
-
     default int maskingValue() {
         return intValue();
     }
